a06498430f3573f2e4519e0de9bd7bfb03b017dc
licii97 <licii97@web.de>
2018-06-25 15:28:19 +0200

    update des Affenlevels

--- Unity/New Unity Project (2)/Assets/ManageScript.cs
+++ Unity/New Unity Project (2)/Assets/ManageScript.cs
@@ -12,22 +12,45 @@ public class ManageScript : MonoBehaviour {
     public GameObject collidedTextObject;
     public string collidedTextObjectName;
     public int endGameCounter = 60;
+    // Vokabeln in je Blatt in drei verschiedene Listen einsortieren 
+    public List<GameObject> firstLeave = new List<GameObject>();
+    public List<GameObject> secondLeave = new List<GameObject>();
+    public List<GameObject> thirdLeave = new List<GameObject>();
+
 
     // Use this for initialization
-    void Start(){}
+    void Start(){
+        SetLeavesActive();
+        InstantiateLists(); 
+        
+    }
 
     // Update is called once per frame
     void Update()
     {
+        
         if (endGameCounter == 0){
             print("Spiel vorbei");
         }
+
         else if (endGameCounter == 40){
+            foreach(GameObject o in secondLeave)
+            {
+                o.SetActive(true);
+            }
+
             Destroy(GameObject.Find("leave 1"));
+            //GameObject.Find("leave 1").SetActive(false);
         }
+
         else if (endGameCounter == 20)
         {
-            Destroy(GameObject.Find("leave 2"));
+            foreach (GameObject o in thirdLeave)
+            {
+                o.SetActive(true);
+            }
+            //Destroy(GameObject.Find("leave 2"));
+            GameObject.Find("leave 2").SetActive(false);
         }
 
         // 0 = linke Maustaste
@@ -73,6 +96,72 @@ public class ManageScript : MonoBehaviour {
         Vector2 mousePos2D = new Vector2(mousePosWorld.x, mousePosWorld.y);
         RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
         return hit;
+    }
+
+    void SetLeavesActive()
+    {
+        GameObject.Find("leave 1").SetActive(true);
+        GameObject.Find("leave 2").SetActive(true);
+        GameObject.Find("leave 3").SetActive(true);
+    }
+
+    void InstantiateLists()
+    {
+        // Vokabeln zu Listen adden
+        //List1 
+        firstLeave.Add(GameObject.Find("apple 2"));
+        firstLeave.Add(GameObject.Find("salad 2"));
+        firstLeave.Add(GameObject.Find("salt 2"));
+        firstLeave.Add(GameObject.Find("milk 2"));
+        firstLeave.Add(GameObject.Find("bread 2"));
+        firstLeave.Add(GameObject.Find("fork 2"));
+        firstLeave.Add(GameObject.Find("grapes 2"));
+        firstLeave.Add(GameObject.Find("pineapple 2"));
+        firstLeave.Add(GameObject.Find("onion 2"));
+        firstLeave.Add(GameObject.Find("cherry 2"));
+
+        foreach (GameObject o in firstLeave)
+        {
+            o.SetActive(true);
+        }
+
+        //List2
+        secondLeave.Add(GameObject.Find("glass 2"));
+        secondLeave.Add(GameObject.Find("pepper 2"));
+        secondLeave.Add(GameObject.Find("butter 2"));
+        secondLeave.Add(GameObject.Find("melon 2"));
+        secondLeave.Add(GameObject.Find("noodles 2"));
+        secondLeave.Add(GameObject.Find("sausage 2"));
+        secondLeave.Add(GameObject.Find("orange 2"));
+        secondLeave.Add(GameObject.Find("plate 2"));
+        secondLeave.Add(GameObject.Find("potato 2"));
+        secondLeave.Add(GameObject.Find("cucumber 2"));
+
+        //ELemente aus Liste inaktiv setzen 
+        foreach (GameObject o in secondLeave)
+        {
+            o.SetActive(false);
+        }
+
+        
+        //List3
+        thirdLeave.Add(GameObject.Find("tomato 2"));
+        thirdLeave.Add(GameObject.Find("cheese 2"));
+        thirdLeave.Add(GameObject.Find("carrot 2"));
+        thirdLeave.Add(GameObject.Find("strawberry 2"));
+        thirdLeave.Add(GameObject.Find("lemon 2"));
+        thirdLeave.Add(GameObject.Find("spoon 2"));
+        thirdLeave.Add(GameObject.Find("knife 2"));
+        thirdLeave.Add(GameObject.Find("cake 2"));
+        thirdLeave.Add(GameObject.Find("cup 2"));
+        thirdLeave.Add(GameObject.Find("banana 2"));
+
+        //ELemente aus Liste inaktiv setzen 
+        foreach (GameObject o in thirdLeave)
+        {
+            o.SetActive(false);
+        }
+                       
     }
 }
 
