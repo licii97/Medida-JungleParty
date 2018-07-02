e70e1f774aa7f8147fbecbbc0df396373339b283
VeronaKolpe <verona@kolpe.de>
2018-07-02 17:44:52 +0200

    highlights für angeklickte wörter im affenlevel

--- Unity/New Unity Project (2)/Assets/Scripts/ManageScript.cs
+++ Unity/New Unity Project (2)/Assets/Scripts/ManageScript.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -16,6 +17,7 @@ public class ManageScript : MonoBehaviour {
     public List<GameObject> helpPage = new List<GameObject>();
     public List<GameObject> pausePage = new List<GameObject>();
     public List<GameObject> gameDonePage = new List<GameObject>();
+    public List<GameObject> textHighlights = new List<GameObject>();
     public ManageLevel manageLevel; 
 
 
@@ -73,17 +75,19 @@ public class ManageScript : MonoBehaviour {
             RaycastHit2D hit = GetHitFromMousePosition(Input.mousePosition);
 
             if (hit.collider != null) {
-                print("Name: " + hit.collider.gameObject.name);
-                print("Tag: " + hit.collider.gameObject.tag);
-
+                //print("Name: " + hit.collider.gameObject.name);
+                //print("Tag: " + hit.collider.gameObject.tag);
                 if (hit.collider.gameObject.tag == "text") {
+                    dishighlightLastObject();
                     SaveTextObject(hit.collider.gameObject);
+                    highlightSavedObject();
                 }
 
                 if (hit.collider.gameObject.tag == "bild") {
                     if (GetNameOfObject(hit.collider.gameObject) == collidedTextObjectName) {
                         Destroy(hit.collider.gameObject);
                         Destroy(collidedTextObject);
+                        Destroy(GetHighlightedObject());
                         endGameCounter -= 2;
                     }
                 }
@@ -122,9 +126,35 @@ public class ManageScript : MonoBehaviour {
         }
     }
 
+    GameObject GetHighlightedObject()
+    {
+        string[] objectName = collidedTextObject.name.Split(' ');
+        int index = Convert.ToInt32(objectName[1]);
+        return textHighlights[index - 1];
+    }
+    void highlightSavedObject()
+    {
+        print("highlighting startet");
+        string[] objectName = collidedTextObject.name.Split(' ');
+        int index = Convert.ToInt32(objectName[1]);
+        print("index: " + index);
+        textHighlights[index-1].SetActive(true);
+        print("highlightObj aus liste: " + textHighlights[index-1]);
+    }
+
+    void dishighlightLastObject()
+    {
+        if (collidedTextObject == null) {
+            print("dishighlight return");
+            return; }
+        string[] objectName = collidedTextObject.name.Split(' ');
+        int index = Convert.ToInt32(objectName[1]);
+        textHighlights[index-1].SetActive(false);
+    }
     void SaveTextObject(GameObject collidedObject)
     {
         collidedTextObject = collidedObject;
+        print("collidedTextObject: " + collidedTextObject);
         string[] objectName = collidedTextObject.name.Split(' ');
         collidedTextObjectName = objectName[0];
     }
@@ -152,6 +182,39 @@ public class ManageScript : MonoBehaviour {
 
     void InstantiateLists()
     {
+        textHighlights.Add(GameObject.Find("apple"));
+        textHighlights.Add(GameObject.Find("salad"));
+        textHighlights.Add(GameObject.Find("salt"));
+        textHighlights.Add(GameObject.Find("milk"));
+        textHighlights.Add(GameObject.Find("bread"));
+        textHighlights.Add(GameObject.Find("fork"));
+        textHighlights.Add(GameObject.Find("grapes"));
+        textHighlights.Add(GameObject.Find("pineapple"));
+        textHighlights.Add(GameObject.Find("onion"));
+        textHighlights.Add(GameObject.Find("cherry"));
+        textHighlights.Add(GameObject.Find("glass"));
+        textHighlights.Add(GameObject.Find("pepper"));
+        textHighlights.Add(GameObject.Find("butter"));
+        textHighlights.Add(GameObject.Find("melon"));
+        textHighlights.Add(GameObject.Find("noodles"));
+        textHighlights.Add(GameObject.Find("sausage"));
+        textHighlights.Add(GameObject.Find("orange"));
+        textHighlights.Add(GameObject.Find("plate"));
+        textHighlights.Add(GameObject.Find("potato"));
+        textHighlights.Add(GameObject.Find("cucumber"));
+        textHighlights.Add(GameObject.Find("tomato"));
+        textHighlights.Add(GameObject.Find("cheese"));
+        textHighlights.Add(GameObject.Find("carrot"));
+        textHighlights.Add(GameObject.Find("strawberry"));
+        textHighlights.Add(GameObject.Find("banana"));
+        textHighlights.Add(GameObject.Find("lemon"));
+        textHighlights.Add(GameObject.Find("spoon"));
+        textHighlights.Add(GameObject.Find("knife"));
+        textHighlights.Add(GameObject.Find("cake"));
+        textHighlights.Add(GameObject.Find("cup"));
+
+        foreach (GameObject o in textHighlights) { o.SetActive(false); }
+
         helpPage.Add(GameObject.Find("anleitung"));
         helpPage.Add(GameObject.Find("anleitungBack"));
 
@@ -169,45 +232,45 @@ public class ManageScript : MonoBehaviour {
 
         // Vokabeln zu Listen adden
         //List1 
-        firstLeave.Add(GameObject.Find("apple 2"));
+        firstLeave.Add(GameObject.Find("apple 1"));
         firstLeave.Add(GameObject.Find("salad 2"));
-        firstLeave.Add(GameObject.Find("salt 2"));
-        firstLeave.Add(GameObject.Find("milk 2"));
-        firstLeave.Add(GameObject.Find("bread 2"));
-        firstLeave.Add(GameObject.Find("fork 2"));
-        firstLeave.Add(GameObject.Find("grapes 2"));
-        firstLeave.Add(GameObject.Find("pineapple 2"));
-        firstLeave.Add(GameObject.Find("onion 2"));
-        firstLeave.Add(GameObject.Find("cherry 2"));
+        firstLeave.Add(GameObject.Find("salt 3"));
+        firstLeave.Add(GameObject.Find("milk 4"));
+        firstLeave.Add(GameObject.Find("bread 5"));
+        firstLeave.Add(GameObject.Find("fork 6"));
+        firstLeave.Add(GameObject.Find("grapes 7"));
+        firstLeave.Add(GameObject.Find("pineapple 8"));
+        firstLeave.Add(GameObject.Find("onion 9"));
+        firstLeave.Add(GameObject.Find("cherry 10"));
 
         foreach (GameObject o in firstLeave) { o.SetActive(true); }
 
         //List2
-        secondLeave.Add(GameObject.Find("glass 2"));
-        secondLeave.Add(GameObject.Find("pepper 2"));
-        secondLeave.Add(GameObject.Find("butter 2"));
-        secondLeave.Add(GameObject.Find("melon 2"));
-        secondLeave.Add(GameObject.Find("noodles 2"));
-        secondLeave.Add(GameObject.Find("sausage 2"));
-        secondLeave.Add(GameObject.Find("orange 2"));
-        secondLeave.Add(GameObject.Find("plate 2"));
-        secondLeave.Add(GameObject.Find("potato 2"));
-        secondLeave.Add(GameObject.Find("cucumber 2"));
+        secondLeave.Add(GameObject.Find("glass 11"));
+        secondLeave.Add(GameObject.Find("pepper 12"));
+        secondLeave.Add(GameObject.Find("butter 13"));
+        secondLeave.Add(GameObject.Find("melon 14"));
+        secondLeave.Add(GameObject.Find("noodles 15"));
+        secondLeave.Add(GameObject.Find("sausage 16"));
+        secondLeave.Add(GameObject.Find("orange 17"));
+        secondLeave.Add(GameObject.Find("plate 18"));
+        secondLeave.Add(GameObject.Find("potato 19"));
+        secondLeave.Add(GameObject.Find("cucumber 20"));
 
         //ELemente aus Liste inaktiv setzen 
         foreach (GameObject o in secondLeave) { o.SetActive(false); }
 
         //List3
-        thirdLeave.Add(GameObject.Find("tomato 2"));
-        thirdLeave.Add(GameObject.Find("cheese 2"));
-        thirdLeave.Add(GameObject.Find("carrot 2"));
-        thirdLeave.Add(GameObject.Find("strawberry 2"));
-        thirdLeave.Add(GameObject.Find("lemon 2"));
-        thirdLeave.Add(GameObject.Find("spoon 2"));
-        thirdLeave.Add(GameObject.Find("knife 2"));
-        thirdLeave.Add(GameObject.Find("cake 2"));
-        thirdLeave.Add(GameObject.Find("cup 2"));
-        thirdLeave.Add(GameObject.Find("banana 2"));
+        thirdLeave.Add(GameObject.Find("tomato 21"));
+        thirdLeave.Add(GameObject.Find("cheese 22"));
+        thirdLeave.Add(GameObject.Find("carrot 23"));
+        thirdLeave.Add(GameObject.Find("strawberry 24"));
+        thirdLeave.Add(GameObject.Find("banana 25"));
+        thirdLeave.Add(GameObject.Find("lemon 26"));
+        thirdLeave.Add(GameObject.Find("spoon 27"));
+        thirdLeave.Add(GameObject.Find("knife 28"));
+        thirdLeave.Add(GameObject.Find("cake 29"));
+        thirdLeave.Add(GameObject.Find("cup 30"));
 
         //ELemente aus Liste inaktiv setzen 
         foreach (GameObject o in thirdLeave) { o.SetActive(false); }
